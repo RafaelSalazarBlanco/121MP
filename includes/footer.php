6c5d2f24bee8d440d4d11bfc60664282dde2cfc5
rafasalazar <frsalazar@grupoasesores.com.mx>
2022-05-12 21:18:06 -0500

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<div style="bottom: 0; width: 100%; position: absolute; background: #2D2D2D;">
+
+            <img src="assets/images/footer.88b9cbba.png" height="100%" align="right" style="padding-left: 20px; padding-top: 20px;">
+        <div class="row" style="position: absolute;">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div
+        </div>
+</div>
---
+++ includes/navbar.php
@@ -0,0 +1,52 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white" style="background: #0b2e13; ">
+    <div style="width: 100%">
+        <div class="row">
+            <div class="col" align="center">
+                <a class="navbar-brand" href="index.php"><img src="assets/images/logo.5c4dbc69.png" width="120"
+                                                              alt="Inicio"></a>
+            </div>
+            <div class="col-8" style="display: grid; align-items: center; padding-right: 80px; padding-left: 80px;">
+                <input type="search" placeholder="Search" aria-label="Search">
+            </div>
+            <div class="col">
+                <div class="row" style="height: 100%;">
+                    <div class="col-8" style="display: grid; align-items: center;">
+                        <a href="#">
+                            Ubicaci&oacute;n <img src="assets/images/location.png" width="20px">  <img src="favicon-96x96.png" width="28px">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Categor&iacute;as</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tiendas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Novedades</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ayuda</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-7"></div>
+            <div class="col">
+                <div class="row" style="height: 100%;">
+                    <div class="col-11" style="display: grid; align-items: center;"><a href="#">Inicia Sesi&oacute;n <img src="assets/images/user-icon.aac87c04.svg" width="20px"></a></div>
+                    <div class="col"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</nav>
