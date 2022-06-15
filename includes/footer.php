c6be61a2aec0f0ade7e20c7bb2e55f1e568b4af6
rafasalazar <frsalazar@grupoasesores.com.mx>
2022-06-15 18:59:29 -0500

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,20 @@
+<div id="modal_contact" class="modal" tabindex="-1">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header" style=" border-bottom: 1px solid #F04648; margin-left: 30px; margin-right: 30px;">
+                <div style="width: 100%; text-align: center; font-family: MontserratSemiBold, serif; font-size: 18px;">Cont&aacute;ctanos</div>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form></form>
+            </div>
+            <div class="modal-footer">
+
+            </div>
+        </div>
+    </div>
+</div>
+
 <div style="bottom: 0; width: 100%; background: #2D2D2D;">
 
     <div class="row" style="width: 100%; padding-top: 25px;">
@@ -8,7 +25,7 @@
                 <li><a class="footerAnchor" href="/admin/files/termsAndConditions.php" target="_blank">T&eacute;rminos y Condiciones</a></li>
                 <li><a class="footerAnchor" href="/admin/files/privacy.php" target="_blank">Pol&iacute;ticas de Privacidad</a></li>
                 <li><a class="footerAnchor" href="mailto:contacto@121marketplace.com?subject=Atención al cliente">Atenci&oacute;n al cliente</a></li>
-                <li><a class="footerAnchor" href="mailto:contacto@121marketplace.com?subject=Contacto">Contacto</a></li>
+                <li><a name="contacto_anchord" class="footerAnchor" href="#">Contacto</a></li>
             </ul>
         </div>
         <div class="col">
@@ -46,3 +63,11 @@
         </div>
     </div>
 </div>
+
+<script>
+    $(document).on("click", "a[name=contacto_anchord]", function (e) {
+        e.preventDefault();
+        $("#modal_contact").modal("show");
+    });
+
+</script>
