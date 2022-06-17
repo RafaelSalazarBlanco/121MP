5699b499edd81d71aa0f6bd60d8ce73fa9db8771
rafasalazar <frsalazar@grupoasesores.com.mx>
2022-06-17 16:29:16 -0500

    fix session

--- includes/validateSession.php
+++ includes/validateSession.php
@@ -7,15 +7,14 @@ $estadoIdentificador = 0;
 $estadoNombre = "Ubicaci&oacute;n";
 $paisNombre = "";
 
+if (isset($_SESSION["estadoIdUser"])) {
+    $estadoIdentificador = $_SESSION["estadoIdUser"];
+}
+
 if (isset($_POST["state"])){
     $_SESSION["estadoIdUser"] = $_POST["state"];
     $estadoIdentificador = $_POST["state"];
 }
-
-if (!isset($_SESSION["estadoIdUser"])) {
-    $estadoIdentificador = $_SESSION["estadoIdUser"];
-}
-
 $resultadoLocations = $new->request("getLocations", array(), "GET");
 $jsonLocations = json_decode($resultadoLocations, true);
 $countries = (null != $jsonLocations && $jsonLocations["countries"]) ? $jsonLocations["countries"] : array();
